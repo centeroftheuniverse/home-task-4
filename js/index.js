cbc3cc538d415851017404d838ba22983dcb0eae
centeroftheuniverse <hardcorenzzz@gmail.com>
2019-11-22 14:43:50 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-function myFunction() {
+function moreB() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+  var btnText = document.getElementById("moreBtn");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
@@ -13,3 +13,39 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+
+
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+
+    /* Slider */
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
